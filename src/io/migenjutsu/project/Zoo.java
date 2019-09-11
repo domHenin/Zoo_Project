45ac7185a1785e60c57fe795f9b8e0dfa0b40b67
domHenin <domhenin.517@gmail.com>
2019-09-11 16:21:07 -0400

    did basic work

--- src/io/migenjutsu/project/Zoo.java
+++ src/io/migenjutsu/project/Zoo.java
@@ -1,29 +1,14 @@
 package io.migenjutsu.project;
 
-public class Zoo {
-    private int yearOpened;
-    private int numOfAnimals;
-    private int numOfWorkers;
-    private int numOfConsumer;
-    private String zooName;
+public abstract class Zoo {
+    private String zooType;
 
-    public Zoo(int yearOpened, int numOfAnimals, int numOfWorkers, int numOfConsumer, String zooName) {
-        setYearOpened(yearOpened);
-        setNumOfAnimals(numOfAnimals);
-        setNumOfWorkers(numOfWorkers);
-        setNumOfConsumer(numOfConsumer);
-        setZooName(zooName);
-    }
+    abstract void numberOfAnimals();
 
-    public void setYearOpened(int yearOpened) { this.yearOpened = yearOpened; }
-    public void setNumOfAnimals(int numOfAnimals) { this.numOfAnimals = numOfAnimals; }
-    public void setNumOfWorkers(int numOfWorkers) { this.numOfWorkers = numOfWorkers; }
-    public void setNumOfConsumer(int numOfConsumer) { this.numOfConsumer = numOfConsumer; }
-    public void setZooName(String zooName) { this.zooName = zooName; }
 
-    public int getYearOpened() { return yearOpened; }
-    public int getNumOfAnimals() { return numOfAnimals; }
-    public int getNumOfWorkers() { return numOfWorkers; }
-    public int getNumOfConsumer() { return numOfConsumer; }
-    public String getZooName() { return zooName; }
+//SETTERS:
+
+
+//GETTERS:
+
 }
